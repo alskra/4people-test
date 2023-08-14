cba7054cf312f013cfb82a226bbf51c650c84906
alskra <krasnoyurchenko.a.s@gmail.com>
2023-08-14 22:44:38 +0300

    Initial commit

---
+++ src/components/app/app.js
@@ -0,0 +1,9 @@
+import Alpine from 'alpinejs';
+// import { disableBodyScroll, enableBodyScroll } from 'body-scroll-lock';
+import './app.scss';
+
+Alpine.data('app', () => ({
+  app: {},
+}));
+
+Alpine.start();
